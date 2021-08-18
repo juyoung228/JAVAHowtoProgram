f30399fcd2c314957e72cfc7c4fa3c127ef6f5d3
juyoung228 <juyoung228@gmail.com>
2021-08-18 23:56:46 +0900

    updated.

---
+++ src/chapter3/Account.java
@@ -0,0 +1,33 @@
+package chapter3;
+
+public class Account{
+    /*Fig.3.1
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+            return name;
+    }
+
+     */
+
+    /*Fig.3.5
+    private String name;
+
+    //constructor initializes name with parameter name
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+     */
+
+}
---
+++ src/chapter3/AccountTest.java
@@ -0,0 +1,34 @@
+package chapter3;
+
+import java.util.Scanner;
+public class AccountTest {
+
+    /*Fig.3.2
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        Account myAccount = new Account();
+        System.out.printf("Initial name is: %s%n%n", myAccount.getName());
+
+        System.out.println("Please enter the name:");
+        String theName = input.nextLine();
+        myAccount.setName(theName);
+
+        System.out.println();
+
+        System.out.printf("Name in object myAccount is:%n%s%n", myAccount.getName());
+    }
+
+     */
+
+    /*Fig.3.6.
+    public static void main(String[] args) {
+        Account account1 = new Account("Jane Green");
+        Account account2 = new Account("John Blue");
+
+        System.out.printf("account1 name is:%s%n", account1.getName());
+        System.out.printf("account2 name is:%s%n", account2.getName());
+    }
+
+
+     */
+}

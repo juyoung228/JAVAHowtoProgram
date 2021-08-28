81428e4e9af5e2d7ead0ee093aec07515829c65f
juyoung228 <juyoung228@gmail.com>
2021-08-28 23:12:48 +0900

    updated.

---
+++ src/chapter5/AutoPolicy.java
@@ -0,0 +1,54 @@
+package chapter5;
+
+//Fig. 5.11
+public class AutoPolicy {
+    private int accountNumber;
+    private String makeAndModel;
+    private String state;
+
+    public AutoPolicy(int accountNumbeer, String makeAndModel, String state) {
+        this.accountNumber = accountNumbeer;
+        this.makeAndModel = makeAndModel;
+        this.state = state;
+
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setMakeAndModel(String makeAndModel) {
+        this.makeAndModel = makeAndModel;
+    }
+
+    public String getMakeAndModel() {
+        return makeAndModel;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public boolean isNoFaultState() {
+        boolean noFaultState;
+
+        switch (getState()) {
+            case "MA": case "NJ": case "NY": case "PA":
+                noFaultState = true;
+                break;
+            default:
+                noFaultState = false;
+                break;
+        }
+
+        return noFaultState;
+    }
+}
